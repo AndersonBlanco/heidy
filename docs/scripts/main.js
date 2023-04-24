199b9d0f08713114c06eb9a4635004f8477d610d
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2023-04-24 15:36:26 +0000

    ander

---
+++ docs/scripts/main.js
@@ -0,0 +1,19 @@
+//alert("website running"); 
+
+
+const wbesiteTitltle_name = 'Heidy'; 
+const header = document.getElementById('header');
+
+var headerContent = `
+<ul>
+
+<li>Home</li>
+<li>Affirmations</l1>
+<li>Contact</l1>
+<li>Shop</l1>
+
+</ul>
+`;
+
+header.innerHTML += headerContent;
+
