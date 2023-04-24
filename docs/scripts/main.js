70c3ce82974e1732f8c429724fb0dc4843455a92
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2023-04-24 17:05:38 +0000

    setup_done

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -3,6 +3,9 @@
 
 const wbesiteTitltle_name = 'Heidy'; 
 const header = document.getElementById('header');
+const content = document.getElementById("content");
+const footer = document.getElementById('footer');
+
 
 var headerContent = `
 <ul>
@@ -15,5 +18,24 @@ var headerContent = `
 </ul>
 `;
 
-header.innerHTML += headerContent;
+//header.innerHTML += headerContent;
 
+function fetching(path, target){
+    fetch(path).then(function (response) {
+	// The API call was successful!
+	return response.text();
+}).then(function (html) {
+     // alert(html)
+	// Convert the HTML string into a document object
+	//var parser = new DOMParser();
+	//var doc = parser.parseFromString(html, 'text/html');
+
+    header.innerHTML += html;
+}).catch(function (err) {
+	// There was an error
+	alert('Something went wrong.', err);
+});}
+
+fetching('./docs/components/header/headerContent.html', header)
+fetching('./docs/components/content/contentCont.html', content)
+fetching('./docs/components/footer/footerCont.html', footer)
